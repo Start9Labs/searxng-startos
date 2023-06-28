036d2432aca793d5a0ef8c04f6e80f90602ed07a
Mariusz Kogen <k0gen@pm.me>
2023-06-28 16:46:28 +0300

    Metrics fix * Latest code from upstream * Code clean

--- scripts/services/migrations.ts
+++ scripts/services/migrations.ts
@@ -1,4 +1,4 @@
 import { compat, types as T } from "../deps.ts";
 
 export const migration: T.ExpectedExports.migration = compat.migrations
-    .fromMapping( { }, "2023.6.26" );
+    .fromMapping( { }, "2023.6.28" );
