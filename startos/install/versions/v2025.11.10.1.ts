92583c4a6f6145aedea5d1fba6bcc2c8a1fb08a1
Mariusz Kogen <k0gen@pm.me>
2025-11-21 16:46:45 +0100

    Updated SDK to beta.44

--- startos/install/versions/v2025.11.10.1.ts
+++ startos/install/versions/v2025.11.10.1.ts
@@ -1,12 +1,12 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
 
 export const v2025_11_10_1 = VersionInfo.of({
-  version: '2025.11.10:1-alpha.0',
+  version: '2025.11.10:1-alpha.1',
   releaseNotes: `\
 ## Update for StartOS 0.4.0
 
 ### Dependency Updates
-* Updated SDK to beta.43
+* Updated SDK to beta.44
 * Updated interface name for backwards compatibility
 * Updated dev dependencies to latest versions
 
